7ee3e2f99c47ef2e607057d562c2109f94423f52
Rupinder Dhillon <rupinder.dhillon@flatironschool.com>
2020-05-13 00:31:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,14 +1,11 @@
-var animal = ""
-
 function myAnimal() {
-  return animal
+  const animal = 'dog';
+  return animal;
 }
 
 function yourAnimal() {
-  // How can we make sure that this function
-  // and the above function both pass?
-  // P.S.: You can't just hard-code 'cat' below
-  return animal
+  const animal = 'cat';
+  return animal;
 }
 
 function add2(n) {
